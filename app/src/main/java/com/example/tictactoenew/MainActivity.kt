01b3c26e0f9538985bc961679bd5cbee0cac4111
Soumya Kota (C) <soumya.kota@rampgroup.com>
2024-01-19 15:14:24 +0530

    TicTacToe Game

---
+++ app/src/main/java/com/example/tictactoenew/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tictactoenew
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var startNewGameButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startNewGameButton = findViewById(R.id.startNewGameBtn)
+
+        startNewGameButton.setOnClickListener {
+            val intent = Intent(this,GameActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tictactoenew/Position.kt
@@ -0,0 +1,3 @@
+package com.example.tictactoenew
+
+data class Position(val row: Int, val column: Int)
